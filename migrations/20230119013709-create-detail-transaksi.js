62afcd2b1725556a72e5b8857cad695d633d38ac
R-twitzy <ramadhanmaulana446@gmail.com>
2023-01-19 09:12:07 +0700

    pp

--- migrations/20230119013709-create-detail-transaksi.js
+++ migrations/20230119013709-create-detail-transaksi.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('detail_transaksis', {
-      id: {
+      id_detail: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
